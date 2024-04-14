aaf5e2ae224bddbdf453efe0b0d6bfc073763078
Christopher Blodgett <christopherblodgett@gmail.com>
2024-04-13 22:11:03 -0700

    fixing image sizes (#51)

--- src/pages/cat.tsx
+++ src/pages/cat.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import {CustomProvider, Container, Nav} from 'rsuite';
+import {Container, Nav} from 'rsuite';
 import CustomNavbar, {NavLink} from '../components/navbar';
 import './index.css';
-
 import cat from '../assets/imgs/alone_cat.png';
 
-function Cat() {
+export default () => {
   return (
-    <CustomProvider>
-      <Container className="App" data-testid="Container">
-        <CustomNavbar />
-        <div className="cat">
-          <Nav className="center">
-            <Nav.Item as={NavLink} href="/">
-              <img src={cat} alt="cat" className="bigCorners" />
-            </Nav.Item>
-          </Nav>
-          <h1 style={{textAlign: 'center'}}>This is a lone cat</h1>
-        </div>
-      </Container>
-    </CustomProvider>
+    <Container className="App" data-testid="Container">
+      <CustomNavbar />
+      <Nav className="center">
+        <Nav.Item as={NavLink} href="/">
+          <img src={cat} alt="cat" className="bigCorners" />
+        </Nav.Item>
+      </Nav>
+      <h1 style={{textAlign: 'center'}}>This is a lone cat</h1>
+    </Container>
   );
-}
-
-export default Cat;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {CustomProvider, Container, Nav} from 'rsuite';
+import {Container, Nav} from 'rsuite';
 import CustomNavbar, {NavLink} from '../components/navbar';
 import './index.css';
 import human_dog from '../assets/imgs/human_dog.png';
@@ -10,35 +10,31 @@ import caged from '../assets/imgs/caged.png';
 
 function Home() {
   return (
-    <CustomProvider>
-      <Container className="App" data-testid="Container">
-        <CustomNavbar />
-        <div className="home">
-          <div className="images">
-            <br />
-            <Nav className="row">
-              <Nav.Item as={NavLink} href="/cat">
-                <img src={alone_cat} alt="cat" className="smallCorners" />
-              </Nav.Item>
-              <Nav.Item as={NavLink} href="/squirrel">
-                <img src={squirrel} alt="squirrel" className="smallCorners" />
-              </Nav.Item>
-              <Nav.Item as={NavLink} href="/tuna">
-                <img src={tuna} alt="tuna" className="smallCorners" />
-              </Nav.Item>
-            </Nav>
-            <Nav className="row">
-              <Nav.Item as={NavLink} href="/caged">
-                <img src={caged} alt="caged" className="smallCorners" />
-              </Nav.Item>
-              <Nav.Item as={NavLink} href="/human_dog">
-                <img src={human_dog} alt="human_dog" className="smallCorners" />
-              </Nav.Item>
-            </Nav>
-          </div>
-        </div>
-      </Container>
-    </CustomProvider>
+    <Container className="App" data-testid="Container">
+      <CustomNavbar />
+      <div className="images">
+        <br />
+        <Nav className="row">
+          <Nav.Item as={NavLink} href="/cat">
+            <img src={alone_cat} alt="cat" className="smallCorners" />
+          </Nav.Item>
+          <Nav.Item as={NavLink} href="/squirrel">
+            <img src={squirrel} alt="squirrel" className="smallCorners" />
+          </Nav.Item>
+          <Nav.Item as={NavLink} href="/tuna">
+            <img src={tuna} alt="tuna" className="smallCorners" />
+          </Nav.Item>
+        </Nav>
+        <Nav className="row">
+          <Nav.Item as={NavLink} href="/caged">
+            <img src={caged} alt="caged" className="smallCorners" />
+          </Nav.Item>
+          <Nav.Item as={NavLink} href="/human_dog">
+            <img src={human_dog} alt="human_dog" className="smallCorners" />
+          </Nav.Item>
+        </Nav>
+      </div>
+    </Container>
   );
 }
 
